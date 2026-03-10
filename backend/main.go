2a45f79469ab6098148f9805b62d74d833043cea
nchakrou <nabilchakroune2@gmail.com>
2026-03-10 15:38:51 +0000

    initialize web project with create-next-app

---
+++ backend/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("server started on port http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
